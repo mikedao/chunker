4ef12ca6d351b42003e9160eb00e6c54ad646f51
Michael Dao <michael.dao@gmail.com>
2015-01-15 12:00:42 -0700

    added more tests

--- chunker_test.rb
+++ chunker_test.rb
@@ -18,9 +18,24 @@ class ChunkerTest < Minitest::Test
     assert_equal ["house"], result
   end
 
+  def test_it_can_handle_leftovers
+    result = Chunker.chunk("houses")
+    assert_equal ["house","s"]
+  end
+
   def test_it_can_split_a_large_string_into_chunks_of_five
     result = Chunker.chunk(@string)
     assert_equal ["Rich ", "think", "s a s", "ingle", " enum", "erabl", "e can", " solv", "e thi", "s."], result
+  end
+
+  def test_it_can_chunk_into_threes
+    result = Chunker.chunk("houses", 3)
+    assert_equal ["hou","ses"]
+  end
+
+  def test_it_can_chunk__into_threes_with_leftovers
+    result = Chunker.chunk("houseses", 3)
+    assert_equal ["hou","ses","es"]
   end
 
 end

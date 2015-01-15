4e4283e26b24d1e038aa6a0f4d8eed9c97ab8e9a
Michael Dao <michael.dao@gmail.com>
2015-01-15 09:09:45 -0700

    initial commit

---
+++ chunker_test.rb
@@ -0,0 +1,26 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative 'chunker.rb'
+
+class ChunkerTest < Minitest::Test
+
+  def setup
+    @string = "Rich thinks a single enumerable can solve this."
+  end
+
+  def test_it_returns_an_array_of_one_when_given_one
+    result = Chunker.chunk("a")
+    assert_equal ["a"], result
+  end
+
+  def test_five_characters_gives_a_single_element
+    result = Chunker.chunk("house")
+    assert_equal ["house"], result
+  end
+
+  def test_it_can_split_a_large_string_into_chunks_of_five
+    result = Chunker.chunk(@string)
+    assert_equal ["Rich ", "think", "s a s", "ingle", " enum", "erabl", "e can", " solv", "e thi", "s."], result
+  end
+
+end
